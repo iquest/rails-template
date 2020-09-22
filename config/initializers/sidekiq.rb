4be8a31f36a22ae57bd21926e50f79cb403d5824
Pavel Dušánek <dusanek@iquest.cz>
2020-09-22 15:47:17 +0200

    Fix sidekiq initializer when Sidekiq not installed

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,9 +1,11 @@
 # frozen_string_literal: true
 
-Sidekiq.configure_server do |config|
-  config.redis = { db: ENV['SIDEKIQ_DB'].presence || '1' }
-end
+if defined?(Sidekiq)
+  Sidekiq.configure_server do |config|
+    config.redis = { db: ENV['SIDEKIQ_DB'].presence || '1' }
+  end
 
-Sidekiq.configure_client do |config|
-  config.redis = { db: ENV['SIDEKIQ_DB'].presence || '1' }
+  Sidekiq.configure_client do |config|
+    config.redis = { db: ENV['SIDEKIQ_DB'].presence || '1' }
+  end
 end
